1d8cfcb8ea3b2ea9568b4f59431236a3b0f3fb57
Spencer Presley <153392395+SpencerPresley@users.noreply.github.com>
2023-12-09 15:45:52 -0500

    Create file1.cpp

---
+++ COSC220/Lab1/file1.cpp
@@ -0,0 +1,15 @@
+//file1.cpp
+//Programmer: Spencer Presley
+//Date: 9/6/22
+#include <iostream>
+#include "file2.h"
+using namespace std;
+
+int main(){
+	int i=3;
+	int sq;
+	
+	sq = Squarelt(i);
+	cout << "Square of " << i << " is " << sq << endl;
+	return 0;
+}
